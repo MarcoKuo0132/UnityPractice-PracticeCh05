51d0ae972721dbae04cce5e7efc342a4611ec46a
MarcoKuo0132 <MarcoKuo0132@github.com>
2020-05-15 15:26:06 +0800

    add character ,ladder and final point

---
+++ Assets/Script/SwitchCollisionEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCollisionEvent : MonoBehaviour
+{
+    public MyCharacter character;
+    bool SwitchEnable = true;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (SwitchEnable)
+        {
+            character.CharacterWalk();
+            SwitchEnable = false;
+        }
+    }
+}
